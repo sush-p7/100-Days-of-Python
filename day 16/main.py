75db7f974b91a431adf96d84383a82e60b93b463
sush <sushh.p.007@gmail.com>
2023-11-12 00:21:09 +0530

    'complete day 16'

--- day 16/main.py
+++ day 16/main.py
@@ -1,12 +1,24 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
-
+is_off = False
 coffeeMachine = CoffeeMaker()
-choice = input(f"What would you like? ({Menu().get_items()}) ")
-if 'report'== choice:
-    coffeeMachine.report()
-else:
+money_machine = MoneyMachine()
+menu = Menu()
+coffeeMachine.report()
+money_machine.report()
+while not is_off:
+    choice = input(f"What would you like? ({menu.get_items()}) ")
+    if choice == 'off':
+        is_off = True
+    elif 'report' == choice:
+        coffeeMachine.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffeeMachine.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffeeMachine.make_coffee(drink)
 
 
 
